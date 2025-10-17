26ee4fdeb2c287faffc6778a454a63b25b9498ae
BanEdward <scho87978@gmail.com>
2025-10-17 16:44:49 +0800

    Menu
    d

--- main.cpp
+++ main.cpp
@@ -40,19 +40,22 @@ string *extractJobDescription(string filteredFileContents[11000], string filepat
 
 int main()
 {
-
-    string listOfFilteredJobDescription[11000];
-    string listOfFilteredResume[11000];
-    cout << "Trimmed Job Requirements: " << endl;
-    extractJobDescription(listOfFilteredJobDescription, "job_description.csv");
-    for (int i = 0; i < 10; i++)
-    {
-        cout << listOfFilteredJobDescription[i] << endl;
-    }
-    cout << "Trimmed Resume Requirements: " << endl;
-    extractJobDescription(listOfFilteredResume, "resume.csv");
-    for (int i = 0; i < 10; i++)
-    {
-        cout << listOfFilteredResume[i] << endl;
-    }
+    // string listOfFilteredJobDescription[11000];
+    // string listOfFilteredResume[11000];
+    // cout << "Trimmed Job Requirements: " << endl;
+    // extractJobDescription(listOfFilteredJobDescription, "job_description.csv");
+    // for (int i = 0; i < 10; i++)
+    // {
+    //     cout << listOfFilteredJobDescription[i] << endl;
+    // }
+    // cout << "Trimmed Resume Requirements: " << endl;
+    // extractJobDescription(listOfFilteredResume, "resume.csv");
+    // for (int i = 0; i < 10; i++)
+    // {
+    //     cout << listOfFilteredResume[i] << endl;
+    // }
+    cout << "Job Matching System\n1. Top 10 best-matched jobs (Rule-based)\n2. Top 10 worst-matched jobs (Rule-based)\n3. Filter jobs for a resume (Rule-based)\n4. Screen reumes for a job (Rule-based)\n5. Boolean search for jobs\n6. Boolean search for resumes\n7. Show all keywords\n8. Add new keyword\n9. Show invalid resumes\n10. Exit\nEnter your choice: ";
+    int choice;
+    cin >> choice;
+    cout << choice << endl;
 }
